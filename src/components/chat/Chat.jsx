4821fe4ad21b6ce8c6b316169707fa176a34ee76
Navneet Nihal <lakranihal0070@gmail.com>
2026-06-09 16:55:27 +0530

    feat(detail): Implement user detail panel UI

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
+
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
 
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
@@ -72,6 +78,7 @@ const Chat = () => {
             <span>1 min ago</span>
           </div>
         </div>
+        <div ref={endRef}></div>
       </div>
          {/* Center div ends here  */}
 
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,65 @@ import "./detail.css"
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jan Doe</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      {/* Info Section */}
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Setting</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+               <div className="photoDetail">
+                 <img src="https://images.pexels.com/photos/5210513/pexels-photo-5210513.jpeg" alt="" />
+                 <span>photo_2026_2.png</span>
+                </div>
+               <img src="./download.png" alt="" className="icon"/>
+            </div>
+            <div className="photoItem">
+               <div className="photoDetail">
+                 <img src="https://images.pexels.com/photos/5210513/pexels-photo-5210513.jpeg" alt="" />
+                 <span>photo_2026_2.png</span>
+               </div>
+               <img src="./download.png" alt="" className="icon"/>
+            </div>
+            <div className="photoItem">
+               <div className="photoDetail">
+                 <img src="https://images.pexels.com/photos/5210513/pexels-photo-5210513.jpeg" alt="" />
+                 <span>photo_2026_2.png</span>
+               </div>
+               <img src="./download.png" alt="" className="icon"/>
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className="logout">Logout</button>
+       </div>
+    </div>
   )
 }
 
